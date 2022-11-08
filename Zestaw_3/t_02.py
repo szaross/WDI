49174e0f806d3cb8230b12a66b4ec42c6ab8774d
szaross <szymonszarek02@gmail.com>
2022-11-08 20:40:08 +0100

    t_02

--- Zestaw_3/t_02.py
+++ Zestaw_3/t_02.py
@@ -1,27 +1,41 @@
-def f(a,b):
-    # a = int(input("Podaj 1. liczbe naturalna: "))
-    # b = int(input("Podaj 2. liczbe naturalna: "))
+def bubble_sort(T):
+    n=len(T)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if T[j]>T[j+1]:
+                T[j],T[j+1]=T[j+1],T[j]
 
-    def dl_liczby(a: int):
+def dl_liczby(a: int):
         licznik=0
         while a!=0:
             a//=10
             licznik+=1
         return licznik
 
-
+def f(a,b):
     sa_zbudowane = True
     tab_a=[0 for _ in range(dl_liczby(a))]
+    tab_b=[0 for _ in range(dl_liczby(b))]
 
     for i in range(dl_liczby(a)):
         tab_a[i]=a%10
         a//=10
 
     for i in range(dl_liczby(b)):
-        if b%10 not in tab_a:
-            sa_zbudowane = False
-            break
+        tab_b[i]=b%10
         b//=10
+
+    if len(tab_b)!=len(tab_a):
+        return False
+
+    bubble_sort(tab_a)
+    bubble_sort(tab_b)
+    
+    for i in range(len(tab_a)):
+        if tab_a[i]!=tab_b[i]:
+            sa_zbudowane=False
 
     return sa_zbudowane
 
+if __name__=="__main__":
+    print(f(123,123))
