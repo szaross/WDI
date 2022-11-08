33a6b7067f950f131b4e443bde0eaff665867864
szaross <szymonszarek02@gmail.com>
2022-11-08 21:18:17 +0100

    t_08

--- Zestaw_3/t_08.py
+++ Zestaw_3/t_08.py
@@ -1,23 +1,23 @@
-def f(tab):
-    def rozklad(a):
+def rozklad(a):
         czynniki = []
-        while a!=1:
+        while a>1:
             for i in range(2,a+1):
                 if a%i==0:
                     czynniki += [i]
                     a//=i
         return czynniki
 
+def f(T):
+    n=len(T)
+    tmp=[False for _ in range(n)]
+    tmp[0]=True
+    for i in range(n):
+        if tmp[i]:
+            czynniki=rozklad(T[i])
+            for c in czynniki:
+                if i+c<n:
+                    tmp[i+c]=True
+    return tmp[n-1]
 
-    def przejscie(i):
-        if i==len(tab)-1:
-            print(True)
-            exit()
-        elif i > len(tab)-1:
-            return False
-        
-        czynniki = rozklad(tab[i])
-        for czynnik in czynniki:
-            przejscie(i+czynnik)
-
-    przejscie(0)
+if __name__=="__main__":
+    print(f([2,2,3,4,5,7]))
