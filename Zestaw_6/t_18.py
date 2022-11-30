57fa88497b5bdaeeeff8f2e4a1daa402872d9305
szaross <szymonszarek02@gmail.com>
2022-11-30 18:03:16 +0100

    t_18

--- Zestaw_6/t_18.py
+++ Zestaw_6/t_18.py
@@ -10,14 +10,18 @@ def num_len(a):
     return counter
 
 def f(T,w=0,k=0): 
+    flag=False
     def krol(T,pv,w=0,k=0):
+        nonlocal flag
         if w>len(T)-1 or k>len(T[0])-1 or w<0 or k<0:
             return False
         if w==len(T)-1 and k==len(T)-1:
+            flag=True
             return True
         if pv>=T[w][k]//(10**(num_len(T[w][k])-1)):
             return False
-        return krol(T,T[w][k]%10,w,k+1) or krol(T,T[w][k]%10,w+1,k+1) or krol(T,T[w][k]%10,w+1,k)
+        if not flag:
+            return krol(T,T[w][k]%10,w,k+1) or krol(T,T[w][k]%10,w+1,k+1) or krol(T,T[w][k]%10,w+1,k)
     
     return krol(T,-inf,w,k)
 
